ab376dca039cc81d36acc8dd4d013ec55ec8c20e
MA-PT <pineda.marvin173@gmail.com>
2021-06-22 20:33:13 -0600

    Ejercicio 1.2 Uploaded

---
+++ Form/Form/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Form.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Form
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void button_Clicked(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(nombre.Text) && !String.IsNullOrWhiteSpace(apellido.Text) && !String.IsNullOrWhiteSpace(edad.Text) && !String.IsNullOrWhiteSpace(correo.Text))
+            {
+                var persona = new Persona
+                {
+                    Name = nombre.Text,
+                    LastName = apellido.Text,
+                    Age = Convert.ToInt32(edad.Text),
+                    Email = correo.Text
+
+                };
+
+                var pag = new Segunda();
+                pag.BindingContext = persona;
+                await Navigation.PushAsync(pag);
+
+            }
+            else
+            {
+                await DisplayAlert("Advertencia", "Llenar todos los campos", "Cerrar");
+            }
+        }
+    }
+}
---
+++ Form/Form/Model/Persona.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Form.Model
+{
+    class Persona
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+
+    }
+}
